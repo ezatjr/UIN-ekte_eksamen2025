a214e7a8144a59f3db3943c27b2bb381a01307ff
ezatjr <Ezatj@hiof.no>
2025-05-19 00:12:58 +0500

    satt opp routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import EventPage from './pages/EventPage';
+import CategoryPage from './pages/CategoryPage';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
     <>
+      <Header />
       <Routes>
-        <Route path="/" element={<div>Home</div>} />
-        <Route path="/about" element={<div>About</div>} />
-        <Route path="/contact" element={<div>Contact</div>} />
-        <Route path="*" element={<div>404 Not Found</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/event/:id" element={<EventPage />} />
+        <Route path="/category/:slug" element={<CategoryPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
+      <Footer />
     </>
   );
 }
